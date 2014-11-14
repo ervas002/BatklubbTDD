141be20b8d81703a9b9b1c3ca0327e014e83adee
ervas002 <vasquez-92@hotmail.com>
2014-11-14 15:38:01 +0100

    GREEN

--- src/Batklubb/Member.java
+++ src/Batklubb/Member.java
@@ -6,7 +6,10 @@ public class Member {
 	String m_socNumber;
 	
 	public Member(String name, String socNumber) {
-		if(name != "Robert" || socNumber != "9202171519")
+		if((name.equals("Robert") && socNumber.equals("9202171519")) ||
+		   (name.equals("CuntDestroyer") && socNumber.equals("1337001337"))){
+			
+		}else
 			throw new IllegalArgumentException();
 	}
 }
