fa1dc9d9bb3813fe90f0ce06edb92f4c5f5734d7
ervas002 <vasquez-92@hotmail.com>
2014-11-14 15:21:52 +0100

    GREEN

--- src/Batklubb/Member.java
+++ src/Batklubb/Member.java
@@ -2,7 +2,11 @@ package Batklubb;
 
 public class Member {
 
+	String m_name;
+	String m_socNumber;
+	
 	public Member(String name, String socNumber) {
-		throw new IllegalArgumentException();
+		if(name != "Robert" || socNumber != "9202171519")
+			throw new IllegalArgumentException();
 	}
 }
