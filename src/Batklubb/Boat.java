0c7075b18a01899a2ec412129a1104fee01f9495
ervas002 <vasquez-92@hotmail.com>
2014-11-20 15:49:16 +0100

    GREEN

--- src/Batklubb/Boat.java
+++ src/Batklubb/Boat.java
@@ -1,9 +1,23 @@
 package Batklubb;
 
 public class Boat {
-	
-	public Boat(BoatType a_bt, BoatSize a_bs){
-		if(a_bt  == null|| a_bs  == null)
-		throw new IllegalArgumentException();
+
+	private BoatType m_boatType;
+	private BoatSize m_boatSize;
+
+	public Boat(BoatType a_bt, BoatSize a_bs) {
+		if (a_bt == null || a_bs == null)
+			throw new IllegalArgumentException();
+
+		m_boatType = a_bt;
+		m_boatSize = a_bs;
+	}
+
+	public BoatType getBoatType() {
+		return m_boatType;
+	}
+
+	public BoatSize getBoatSize() {
+		return m_boatSize;
 	}
 }
