8485ca82414ced8eff8e6dd7b734b486f9642e6b
ervas002 <vasquez-92@hotmail.com>
2014-11-21 17:00:45 +0100

    BLUE

--- src/Batklubb/IOmanager.java
+++ src/Batklubb/IOmanager.java
@@ -5,10 +5,18 @@ import java.util.Scanner;
 public class IOmanager {
 	
 	public String getNameInput(){
-		return "";
+		return readInput();
 	}
 	
 	public String getSocNumInput(){
-		return "";
+		return readInput();
+	}
+	
+	public String readInput()
+	{
+		Scanner sc = new Scanner(System.in);
+		String input = sc.nextLine();
+		sc.close();
+		return input;
 	}
 }
