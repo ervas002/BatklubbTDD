a1f27a8dd7bbfb580a666b61e7fac76a45f25721
Robert Forsgren <rf222ce@student.lnu.se>
2014-11-25 18:06:45 +0100

    GREEN

--- src/Batklubb/PaymentManager.java
+++ src/Batklubb/PaymentManager.java
@@ -2,6 +2,14 @@ package Batklubb;
 
 public class PaymentManager {
 
+	public void makePayment(Member a_member, Boat a_boat) {
+		if(a_member == null || a_boat == null)
+		{
+			throw new IllegalArgumentException();
+		}
+		
+	}
+
 //	public boolean HasPayed(Member a_member, Boat a_boat) {
 //		
 //		// CONNECT TO THE "DATABASE", CHECK IF a_member has payed for a_boat.
